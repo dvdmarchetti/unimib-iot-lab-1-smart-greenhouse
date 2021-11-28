e921762fa322a9b6abde1705a2500496383061ad
Davide Marchetti <davide.marchetti.mail@gmail.com>
2021-11-28 16:03:38 +0100

    Strip password

---
+++ includes/secrets.h
@@ -0,0 +1,12 @@
+// WiFi
+#define SECRET_SSID "YourNetwork"
+#define SECRET_PASS "YourPassword"
+
+// InfluxDB
+#define INFLUXDB_URL "http://149.132.178.180:8086"
+// API Token (UI > Load Data > Tokens)
+#define INFLUXDB_TOKEN "AoWqj1WemC1WCMPMdYnBFxcjtlX3gqlCIY6wLXWVrEyolm7E08R-EVEyUtRb6YS4XWogTCd5vXmjt_uS8Vcvxg=="
+// Organization ID (UI > Settings > Profile)
+#define INFLUXDB_ORG "labiot-org"
+// Bucket name (UI > Load Data > Buckets)
+#define INFLUXDB_BUCKET "dmarchetti8-bucket"
